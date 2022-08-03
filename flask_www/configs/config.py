896875ca093a488b366d7c65f823c228e50a7d1a
user <moljin@naver.com>
2022-08-03 15:38:59 +0900

    static/media folder = finish

--- flask_www/configs/config.py
+++ flask_www/configs/config.py
@@ -34,7 +34,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 class DevelopmentConfig(Config):
