7727c66b176f0969f17f4c50e68e30be755da9af
user <moljin@naver.com>
2022-08-03 16:14:11 +0900

    detail and list page

--- flask_www/configs/apps.py
+++ flask_www/configs/apps.py
@@ -1,4 +1,13 @@
 def related_app(app):
+    @app.template_filter('daytime')
+    def _format_datetime(value, _type=None):  # 템플릿단 얘시: what_date|daytime("full")
+        if _type == "full":
+            _format = '%Y-%m-%d %H:%M:%S'
+        elif _type == "medium":
+            _format = '%Y-%m-%d %H:%M'
+        else:
+            _format = '%Y-%m-%d'
+        return value.strftime(_format)
 
     @app.template_filter('ko_datetime')
     def korean_datetime(value, fmt='%Y년 %m월 %d일 %p %I:%M'):
